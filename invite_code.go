c0a2843d465255d944b0ab25b986a7c5028991af
betdev <fbg@live.com>
2018-12-10 16:30:03 +0800

    Update invite_code.go

--- invite_code.go
+++ invite_code.go
@@ -7,18 +7,18 @@ import (
 )
 
 const (
-	BASEID = int64(1000000000)
+	BASEID = uint64(1000000000)
 )
 
 //UserId 转 唯一邀请码
-func GetInviteCode(userId int64) (invitecode string) {
+func GetInviteCode(userId uint64) (invitecode string) {
 	invitecode = bases.ToBase(int(userId+BASEID), 58)
 	return
 }
 
 //唯一邀请码 转 UserId
-func GetUserId(inviteCode string) (int64, error) {
-	userid := int64(bases.FromBase(inviteCode, 58)) - BASEID
+func GetUserId(inviteCode string) (uint64, error) {
+	userid := uint64(bases.FromBase(inviteCode, 58)) - BASEID
 	if userid < 0 {
 		return -1, errors.New("invalid invite code")
 	}
